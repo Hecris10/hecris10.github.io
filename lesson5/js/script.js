7ed2c44850c33684c32e528f733c30bed2f87164
Hecris10 <helamaewerton@gmail.com>
2021-10-20 11:00:26 -0300

    updating menu

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -57,7 +57,10 @@ if (n == "Satuday") {
     document.querySelector("#message-top").style.display = "block";
 }
 
-function toggleMenu() {
-    console.log(document.getElementById("menu").classList);
-    document.getElementById("menu").classList.toggle("hide");
-}
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => { if (window.innerWidth > 760) mainnav.classList.remove('responsive') };
